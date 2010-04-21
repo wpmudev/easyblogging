eb46dd3e143c4ca92a7aa45278accf762c70c588
jason <jason@f53fe46e-8c02-459b-9fdd-93e75771b278>
2010-04-21 03:48:09 +0000

    Added to enhance navigation between Easy & Advanced areas, as well as provide a default page for functions not available in the Easy admin area, like plugin maintenance
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/easyblogging@272 f53fe46e-8c02-459b-9fdd-93e75771b278

---
+++ easybloggingfiles/noteasy.php
@@ -0,0 +1,34 @@
+<?php
+    require_once(ABSPATH . 'wp-admin/admin.php');
+    require_once(ABSPATH . 'wp-admin/includes/admin.php');
+    
+    $title = __('Whoops!', $this->localizationDomain);
+
+    remove_action( 'admin_footer', 'bp_core_admin_bar');
+?>
+<script type="text/javascript">
+    var href = '<?php echo admin_url('%%replace%%.php') . '?easyadmin=off'; ?>';
+    if (querystring != '') {
+        href += '&' + querystring;
+    }
+    jQuery("#to_advanced_page.noteasy").attr('href',href.replace('%%replace%%',advancedpage));
+</script>
+<style type="text/css">
+    .error { display: none; }
+</style>
+   <div class="wrap">
+        <?php screen_icon(); ?>
+        <h2><?php echo esc_html( $title ); ?></h2>
+        <div id="welcome-area">
+            <p><?php _e( 'Whoops! This function is not available in the Easy Admin area. You\'ll have to switch over to the Advanced Admin area to access this page.', $this->localizationDomain ) ?></p>
+            <div id="admin_area_step_new_post" class="admin_area_step">
+                <a id="to_advanced_page" class='wizard_button noteasy' href="<?php echo admin_url('%%replace%%.php?easyadmin=off');?>" target="_top">
+                    <div id="admin_area_to_advanced" class="admin_area button">
+		            <?php _e( 'Go to the Advanced Admin Area', $this->localizationDomain ) ?>
+	            </div></a>
+                <div class="clear"></div>
+            </div>
+            <div class="clear"></div>
+            <p>&nbsp;</p>
+        </div>
+    </div>
